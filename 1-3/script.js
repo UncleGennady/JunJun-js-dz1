7b26289c27054286220796666369df367a087b8f
Aleksandr Protasenko <96978077+UncleGennady@users.noreply.github.com>
2022-02-15 11:28:37 +0200

    dz-1

---
+++ 1-3/script.js
@@ -0,0 +1,11 @@
+'use strict';
+let admin;
+let name = 'Джон';
+admin = name;
+alert(admin);
+//---------------------------------------------
+let ourPlanet = 'Земля';
+let currentUser = 'ПетяВася';
+//---------------------------------------------
+let number = '10' + 10;
+console.log(typeof number); // из-за того что к строке прибавляеться число
